dfce3bfbf93ae23f31844ff7a1565dc0ca0b102a
Jorge Kas-danouche <jkasdanouche@gmail.com>
2021-04-14 13:07:38 -0700

    fixing bugs.

--- kali2021-theme/install.sh
+++ kali2021-theme/install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-configDir = ~/
-configFile = ${configDir}.bashrc 
+configDir=$HOME
+configFile=${configDir}/.bashrc
+backupDir=$PWD
+backupFile=.bashrc.bak
 echo "Searching .bashrc configuration file..."
-if [ -f "${configFile}"]; then
+if  test -f "$configFile"; then
         echo "File found."
         echo "Backing up configuration file."
-	cp ${configFile} ./.bashrc.bak
+	cp ${configFile} ${backupDir}/${backupFile}
 	mv ${configFile} ${conigFile}.bak
+	test -f "${backupDir}/${backupFile}" && echo "Backup done at: "${backupDir}/${backupFile} || echo "E: Backup could not be made."
 	echo "Adding new configuration file."
-	cp ./.bashrc ${configDir}
+	cp $PWD/.bashrc ${configDir}
 	echo "Done."
 else
-	echo "file not found."
+	echo "File not found."
 	echo "Adding new configuration file."
 	cp ./.bashrc ${configDir}
 	echo "Done."
