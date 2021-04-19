bebbe54ab24c638a0e0fccf81a1edf48d406f87f
Jorge Kas-danouche <jkasdanouche@gmail.com>
2021-04-19 12:24:56 -0700

    Installation script

---
+++ fancy-powerline/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##  By: Jorge Kas-danouche <jkasdanouche@gmail.com>
+##  This scritp backups the original configuration and install the bash theme.
+
+configDir=$HOME
+configFile=${configDir}/.bashrc
+backupDir=$PWD
+backupFile=.bashrc.bak
+echo "Searching .bashrc configuration file..."
+if  test -f "$configFile"; then
+        echo "File found."
+        echo "Backing up configuration file."
+	cp ${configFile} ${backupDir}/${backupFile}
+	mv ${configFile} ${conigFile}.bak
+	test -f "${backupDir}/${backupFile}" && echo "Backup done at: "${backupDir}/${backupFile} || echo "E: Backup could not be made."
+	echo "Adding new configuration file."
+	cp $PWD/.bashrc ${configDir}
+	echo "Done."
+else
+	echo "File not found."
+	echo "Adding new configuration file."
+	cp ./.bashrc ${configDir}
+	echo "Done."
+
+fi
+
